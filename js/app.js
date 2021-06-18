9d5faaa463ebd775784ae289b0f38eb93b3d8dc2
Alejandra Valdivia <alexxandra_valdivia@hotmail.com>
2021-06-18 11:20:52 -0700

    Updated image alt description

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const select = document.getElementById('breeds');
-const card = document.querySelector('.card'); 
+const card = document.querySelector('.card');
 const form = document.querySelector('form');
 
 // ------------------------------------------
@@ -8,22 +8,22 @@ const form = document.querySelector('form');
 
 function fetchData(url) {
   return fetch(url)
-  .then(checkStatus)
-  .then(res => res.json())
-  .catch(error => console.log('Looks like there was a problem', error))
+    .then(checkStatus)
+    .then(res => res.json())
+    .catch(error => console.log('Looks like there was a problem', error))
 }
 
 Promise.all([
   fetchData('https://dog.ceo/api/breeds/list'),
   fetchData('https://dog.ceo/api/breeds/image/random')
 ])
-.then(data => {
-  const breedList = data[0].message;
-  const randomImage = data[1].message;
-  
-  generateOptions(breedList);
-  generateImage(randomImage);
-})
+  .then(data => {
+    const breedList = data[0].message;
+    const randomImage = data[1].message;
+
+    generateOptions(breedList);
+    generateImage(randomImage);
+  })
 
 
 
@@ -35,20 +35,21 @@ Promise.all([
 function checkStatus(response) {
   if (response.ok) {
     return Promise.resolve(response);
-  }else {
-    return Promise.reject(new Error(response.statusText)); 
+  } else {
+    return Promise.reject(new Error(response.statusText));
   }
 }
 
 function generateOptions(data) {
   const options = data.map(item => `
-      <option value='${item}'>${item}</option>         `).join('');
-   select.innerHTML = options;                     
+      <option value='${item}'>${item}</option>         
+      `).join('');
+  select.innerHTML = options;
 }
 
 function generateImage(data) {
   const html = `
-    <img src='${data}' alt>
+    <img src='${data}' alt="picture of a dog">
     <p>Click to view images of ${select.value}s</p>
 `;
   card.innerHTML = html;
@@ -59,11 +60,11 @@ function fetchBreedImage() {
   const img = card.querySelector('img');
   const p = card.querySelector('p');
   fetchData(`https://dog.ceo/api/breed/${breed}/images/random`)
-  .then(data => {
-    img.src = data.message;
-    img.alt = breed;
-    p.textContent = `Click to view more ${breed}s`;
-  })
+    .then(data => {
+      img.src = data.message;
+      img.alt = breed;
+      p.textContent = `Click to view more ${breed}s`;
+    })
 }
 
 // ------------------------------------------
@@ -79,9 +80,9 @@ form.addEventListener('submit', postData);
 
 function postData(e) {
   e.preventDefault();
-  const name =  document.getElementById('name').value;
+  const name = document.getElementById('name').value;
   const comment = document.getElementById('comment').value;
-  
+
   const config = {
     method: 'POST',
     headers: {
@@ -89,11 +90,11 @@ function postData(e) {
     },
     body: JSON.stringify({ name, comment })
   }
-  
+
   fetch('https://jsonplaceholder.typicode.com/comments', config)
-  .then(checkStatus)
-  .then(res => res.json())
-  .then(data => console.log(data))
+    .then(checkStatus)
+    .then(res => res.json())
+    .then(data => console.log(data))
 }
 
 
